5b963e50b38bc9c4fa14862ada375a6d5e460c33
FJ <rzrtwt@gmail.com>
2024-07-16 08:51:44 +0200

    relative path scripts

--- ffmpeg-api.py
+++ ffmpeg-api.py
@@ -23,33 +23,6 @@ class Item(BaseModel):
     price: float
     is_offer: Union[bool, None] = None
 
-
-#conversion endpoint POST method
-##Post model definition - defines expected json structure
-class Post(BaseModel):
-    post_id: int
-    image_url: str
-    arg3: str
-
-##conversion endpoint
-##expects Post model json structure as argument (data sent in request)
-@app.get("/convert/image-to-video")
-def post_convert(payload: Post):
-
-    #runs prepare-dir.sh script with only argument being the directory name = payload.post_id
-    subprocess.run(["/home/ubuntu/one-click-hello/ffmpeg-api/prepare-dir.sh", f"{payload.post_id}"])
-
-    #download the image from {image_url}
-    img_data = requests.get(payload.image_url).content
-    with open(f"converted/{payload.post_id}/{payload.post_id}.jpg", 'wb') as file:
-        file.write(img_data)
-
-    #runs ffmpeg-convert.sh script to convert the DLed image to .mp4
-    subprocess.run(["/home/ubuntu/one-click-hello/ffmpeg-api/ffmpeg-convert.sh", f"{payload.post_id}"])
-
-    #pass the video file as a response
-    return fastapi.responses.FileResponse(f"/home/ubuntu/one-click-hello/ffmpeg-api/converted/{payload.post_id}/{payload.post_id}.mp4")
-
 #example conversion endpoint method with separate args, returns json
 @app.get("/example")
 def convert_image_to_video(post_id: int, image_url: str, arg2: Union[str,None] = "None"):
@@ -68,4 +41,31 @@ def convert_image_to_video(post_id: int, image_url: str, arg2: Union[str,None] =
             "arg2": arg2
         }
     }
+
+
+#CONVERSION ENDPOINT BEGINS HERE
+##Post model definition - defines expected json structure
+class Post(BaseModel):
+    post_id: int
+    image_url: str
+    arg3: str
+
+##conversion endpoint
+##expects Post model json structure as argument (data sent in request)
+@app.get("/convert/image-to-video")
+def post_convert(payload: Post):
+
+    #runs prepare-dir.sh script with only argument being the directory name = payload.post_id
+    subprocess.run(["./prepare-dir.sh", f"{payload.post_id}"])
+
+    #download the image from {image_url}
+    img_data = requests.get(payload.image_url).content
+    with open(f"converted/{payload.post_id}/{payload.post_id}.jpg", 'wb') as file:
+        file.write(img_data)
+
+    #runs ffmpeg-convert.sh script to convert the DLed image to .mp4
+    subprocess.run(["./ffmpeg-convert.sh", f"{payload.post_id}"])
+
+    #pass the video file as a response
+    return fastapi.responses.FileResponse(f"./converted/{payload.post_id}/{payload.post_id}.mp4")
 
--- prepare-dir.sh
+++ prepare-dir.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # makes a directory with a arg1 ($1, input at invocation) as a name
-mkdir /home/ubuntu/one-click-hello/ffmpeg-api/converted/$1
+mkdir -p ./converted/$1
